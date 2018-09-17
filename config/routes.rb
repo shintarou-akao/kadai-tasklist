be95c559a8e100d124fc3127e3911c0ef4f2167b
shintarou.akao <xftap2e4c4g@yahoo.co.jp>
2018-09-17 07:35:14 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
-  get 'toppages/index' #rails g controller toppages indexによって生成された
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+
+  get 'toppages/index' # rails g controller toppages indexによって生成された
 
   root to: 'tasks#index'
-  root to: "toppages#index"
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
