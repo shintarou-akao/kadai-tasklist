9ac40de4cfab440d0d9687a45f6a80a48fac75b3
shintarou.akao <xftap2e4c4g@yahoo.co.jp>
2018-09-19 09:06:36 +0000

    resubmit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in # lesson9.4で追加
-  before_action :correct_user, only: [:destroy] # lesson9.5で追加
+  before_action :correct_user, only: [:show, :edit, :update, :destroy] # lesson9.5で追加
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    else
+      redirect_to login_url
+    end
   end
   
   def show
@@ -17,16 +21,14 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     # @task = Task.new(task_params)
-    @task.user = current_user #新規タスク作成出来なかったので追加した
+    # @task.user = current_user 
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      # redirect_to @task
       redirect_to root_url
     else
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした'
-      # render :new
       render 'toppages/index'
     end
   end
@@ -35,7 +37,6 @@ class TasksController < ApplicationController
   end
   
   def update
-    
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
@@ -47,17 +48,11 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
-    # redirect_to tasks_url
     redirect_back(fallback_location: root_path)
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,16 @@
 Rails.application.routes.draw do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  
-    get 'users/index'
-    get 'users/show'
-    get 'users/new'
-    get 'users/create'
+    
+    # get 'toppages/index' # rails g controller toppages indexによって生成された
 
-    get 'toppages/index' # rails g controller toppages indexによって生成された
-
-  root to: 'tasks#index'
-  root to: 'toppages#index'
+    # root to: 'tasks#index'
+    root to: 'toppages#index'
   
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
-  resources :tasks #, only: [:create, :destroy]
-  resources :users, only: [:index, :show, :new, :create]
+    get 'signup', to: 'users#new'
+    resources :tasks
+    resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
